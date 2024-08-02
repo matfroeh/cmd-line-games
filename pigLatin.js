791951250dbe100c54affe05324d4a0d5e3d336e
Matthias Froehlich <froehlich_matthias@yahoo.com>
2024-08-02 20:16:53 +0200

    setting up user input

--- pigLatin.js
+++ pigLatin.js
@@ -1,3 +1,7 @@
 // English to Pig Latin Translator
 
 const args = process.argv.slice(2);
+
+// Fetching user input as an array with each word as a separate element
+const userInput = args;
+// console.log(args);
