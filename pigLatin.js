7d1a49fc3afd36e4aba8a26f1f22f79ac2c1d3f8
Matthias Froehlich <froehlich_matthias@yahoo.com>
2024-08-02 20:51:15 +0200

    fix in formating input

--- pigLatin.js
+++ pigLatin.js
@@ -5,12 +5,12 @@ const args = process.argv.slice(2);
 // Fetching user input as an array with each word as a separate element
 const rawUserInputArray = args;
 
-let userInput = [];
+// Format each word to lower case letters into new array
+let formattedUserInput = [];
 rawUserInputArray.forEach(element => {
-  userInput.push(element.toLowerCase());
+  formattedUserInput.push(element.toLowerCase());
 });
-
-console.log(userInput);
+console.log(formattedUserInput);
 
 // *toDO* Function that takes a string (word) as input and transforms it to pig latin
 let translateToPigLatin = function (word) {
