d6adc87bf177f677bc39406a17ff7fca29b2404a
Matthias Froehlich <froehlich_matthias@yahoo.com>
2024-08-02 21:19:36 +0200

    added checkTypeOfWord function

--- pigLatin.js
+++ pigLatin.js
@@ -7,10 +7,10 @@ const rawUserInputArray = args;
 
 // Format each word to lower case letters into new array
 let formattedUserInput = [];
-rawUserInputArray.forEach(element => {
+rawUserInputArray.forEach((element) => {
   formattedUserInput.push(element.toLowerCase());
 });
-console.log(formattedUserInput);
+// console.log(formattedUserInput);
 
 // *toDO* Function that takes a string (word) as input and transforms it to pig latin
 let translateToPigLatin = function (word) {
@@ -19,8 +19,25 @@ let translateToPigLatin = function (word) {
   return translateToPigLatin;
 };
 
+const regExVowel = /[aeiouy]/g;
+
 // *toDo* Word Checker checks to which case the input word corresponds to
 let checkTypeOfWord = function (word) {
-  if (word.CharAt(0) !== ("a") )
+  // Starts with Consonant + Vowel
+  if (
+    word.charAt(0).search(regExVowel) == -1 &&
+    word.charAt(0).search(regExVowel) != -1
+  ) {
+    return "consonantPlusVowel";
+  } else if (
+    word.charAt(0).search(regExVowel) == -1 &&
+    word.charAt(0).search(regExVowel) != -1
+  ) {
+    return "consonantPlusConsonant";
+  } else if (word.charAt(0).search(regExVowel) != -1) {
+    return "vowel";
+  } else {
+    console.log("Error. Word starts with irregular letters.");
     return;
+  }
 };
