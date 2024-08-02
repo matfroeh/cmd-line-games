9e11e0a7893bcabcc782938f036f8c5aede3f2e8
Matthias Froehlich <froehlich_matthias@yahoo.com>
2024-08-02 20:49:17 +0200

    added user input formatting

--- pigLatin.js
+++ pigLatin.js
@@ -3,5 +3,24 @@
 const args = process.argv.slice(2);
 
 // Fetching user input as an array with each word as a separate element
-const userInput = args;
-// console.log(args);
+const rawUserInputArray = args;
+
+let userInput = [];
+rawUserInputArray.forEach(element => {
+  userInput.push(element.toLowerCase());
+});
+
+console.log(userInput);
+
+// *toDO* Function that takes a string (word) as input and transforms it to pig latin
+let translateToPigLatin = function (word) {
+  // *toDO* Code
+
+  return translateToPigLatin;
+};
+
+// *toDo* Word Checker checks to which case the input word corresponds to
+let checkTypeOfWord = function (word) {
+  if (word.CharAt(0) !== ("a") )
+    return;
+};
