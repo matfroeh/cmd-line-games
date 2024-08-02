186a682e9023e857adca91de8ca460a101480c83
Matthias Froehlich <froehlich_matthias@yahoo.com>
2024-08-02 21:44:43 +0200

    added translator function

--- pigLatin.js
+++ pigLatin.js
@@ -5,7 +5,7 @@ const args = process.argv.slice(2);
 // Fetching user input as an array with each word as a separate element
 const rawUserInputArray = args;
 
-// Format each word to lower case letters into new array
+// Format each word to lower case letters into new array *ToDO* undo formatting, information of upper or lower case needed later
 let formattedUserInput = [];
 rawUserInputArray.forEach((element) => {
   formattedUserInput.push(element.toLowerCase());
@@ -13,31 +13,46 @@ rawUserInputArray.forEach((element) => {
 // console.log(formattedUserInput);
 
 // *toDO* Function that takes a string (word) as input and transforms it to pig latin
-let translateToPigLatin = function (word) {
-  // *toDO* Code
+let translatedWordToPigLatin = function (word) {
+  translatedWordToPigLatin = "";
 
-  return translateToPigLatin;
+  switch (checkTypeOfWord(word)) {
+    case "consonantPlusVowel":
+      translatedWordToPigLatin = word.slice(1) + word.at(0) + "ay";
+      break;
+    case "consonantPlusConsonant":
+      translatedWordToPigLatin = word.slice(2) + word.slice(0, 2) + "ay";
+      break;
+    case "vowel":
+      translatedWordToPigLatin = word + "way";
+      break;
+    default:
+      console.log("Error during translation.");
+  }
+
+  return translatedWordToPigLatin;
 };
 
 const regExVowel = /[aeiouy]/g;
 
 // *toDo* Word Checker checks to which case the input word corresponds to
 let checkTypeOfWord = function (word) {
-  // Starts with Consonant + Vowel
   if (
     word.charAt(0).search(regExVowel) == -1 &&
-    word.charAt(0).search(regExVowel) != -1
+    word.charAt(1).search(regExVowel) != -1
   ) {
     return "consonantPlusVowel";
   } else if (
     word.charAt(0).search(regExVowel) == -1 &&
-    word.charAt(0).search(regExVowel) != -1
+    word.charAt(1).search(regExVowel) == -1
   ) {
     return "consonantPlusConsonant";
   } else if (word.charAt(0).search(regExVowel) != -1) {
     return "vowel";
   } else {
     console.log("Error. Word starts with irregular letters.");
-    return;
+    return -1;
   }
 };
+
+return console.log(translatedWordToPigLatin(formattedUserInput[0]));
