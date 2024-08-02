f14eec6d32ae27c1eedec3f92ff01beeb5b465f1
Matthias Froehlich <froehlich_matthias@yahoo.com>
2024-08-02 23:20:02 +0200

    Final Version. Only letters allowed, no punctuation, dashes etc.

--- pigLatin.js
+++ pigLatin.js
@@ -8,7 +8,7 @@ const rawUserInputArray = args;
 // Case: User input is a single string
 const rawUserInputString = args[0];
 
-const formattedUserInput= rawUserInputString.split(" ");
+const formattedUserInput = rawUserInputString.split(" ");
 
 // Old: in this way, the user input is thought to be a list of arguments with no ""-enclosure of the whole sentence
 // // Format each word to lower case letters into new array *ToDO* undo formatting, information of upper or lower case needed later
@@ -18,7 +18,7 @@ const formattedUserInput= rawUserInputString.split(" ");
 // });
 
 // matches vowel - case sensitive
-const regExVowel = /[aeiouy]/g;
+const regExVowel = /[aeiouy]/gi;
 
 // Word Checker checks to which case the input word corresponds to
 const checkTypeOfWord = function (word) {
@@ -58,6 +58,14 @@ let translateWord = function (word) {
     default:
       console.log("Error during translation.");
   }
+
+  // Rearange the upper case letter in case the word started with one
+  regExUpperCaseLetter = /[A-Z]/;
+  if (wordTranslated.search(regExUpperCaseLetter) != -1) {
+    wordTranslated = wordTranslated.toLowerCase();
+    wordTranslated = wordTranslated.at(0).toUpperCase() + wordTranslated.slice(1);
+  }
+
   return wordTranslated;
 };
 
