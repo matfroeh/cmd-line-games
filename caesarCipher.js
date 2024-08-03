5380392254a325ec4991dade1057285190937edb
Matthias Froehlich <froehlich_matthias@yahoo.com>
2024-08-03 17:01:34 +0200

    Caesar initial js

---
+++ caesarCipher.js
@@ -0,0 +1,7 @@
+// Caesar Cipher
+
+const args = process.argv.slice(2);
+
+// User input is supposed to be a single string argument
+const rawUserInputString = args[0];
+const formattedUserInput = rawUserInputString.split(" ");
